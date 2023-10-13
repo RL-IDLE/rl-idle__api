7a4dc1cd8dedc160199819613ffc836081f7f0d2
Thrive <guillaumemengelatte@hotmail.fr>
2023-10-13 10:43:05 +0200

    test: web socket test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,53 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { EventsModule } from './events/events.module';
+
+const isProduction = process.env.NODE_ENV === 'production';
+console.log('isProductionDB', isProduction);
+
+
+const typeOrmConfig: TypeOrmModuleOptions = isProduction
+  ? {
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      ssl: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+      // extra: {
+      //   min: 2,
+      //   max: 5
+      // }
+    }
+  : {
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+      // extra: {
+      //   min: 2,
+      //   max: 5
+      // }
+    };
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
